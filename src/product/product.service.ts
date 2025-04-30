db3b3b53f3342760b4b14931543d040e337ddf12
Abdelrahman Kamel Elgendy <158625675+abdelrahman-kamel-elgendy@users.noreply.github.com>
2025-04-30 11:41:01 +0300

    fix: correct import path for ProductRepository in ProductService

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Product } from '../../node_modules/.prisma/client'; 
 import { BaseService } from '../base/baseService';
 import * as productDto from "./product.dto";
-import { ProductRepository } from './category.repository';
+import { ProductRepository } from './product.repository';
 
 @Injectable()
 export class ProductService extends BaseService<Product, productDto.CreateProductDTO, productDto.UpdateProductDTO> {
